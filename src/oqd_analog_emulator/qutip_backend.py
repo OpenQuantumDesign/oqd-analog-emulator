f1a214b36182ead06b62179f87102955bf8ace6d
vishnu-m77 <vmugunda@uwaterloo.ca>
2026-07-22 13:16:42 -0400

    partial feat: Use rewrite rule instead of conversion rule to lower analog AST to QuTip. Tests unfixed

--- src/oqd_analog_emulator/qutip_backend.py
+++ src/oqd_analog_emulator/qutip_backend.py
@@ -16,18 +16,12 @@ from oqd_core.backend.base import BackendBase
 from oqd_core.backend.task import Task
 
 from oqd_analog_emulator.passes import (
-    run_qutip_experiment,
-    compiler_analog_args_to_qutipIR,
-    compiler_analog_circuit_to_qutipIR,
+    # run_qutip_experiment,
+    compiler_analog_circuit_to_qutip_backend,
 )
 
-from oqd_core.compiler.analog.passes.canonicalize import (
-    analog_operator_canonicalization,
-)
-from oqd_core.compiler.analog.passes.assign import (
-    assign_analog_circuit_dim,
-    verify_analog_args_dim,
-)
+from oqd_core.compiler.analog.passes.compile import compile_analog_circuit
+
 
 ########################################################################################
 
@@ -41,53 +35,7 @@ __all__ = [
 class QutipBackend(BackendBase):
     """
     Class representing the Qutip backend
-    """
-
-    def compile(self, task: Task):
-        """
-        Method for compiling program of task to a [`QutipExperiment`][oqd_analog_emulator.interface.QutipExperiment] and converting
-        args of task to [`TaskArgsAnalog`][oqd_core.backend.task.TaskArgsAnalog].
-
-        Args:
-            task (Task): Quantum experiment to compile
-
-        Returns:
-            converted_circuit (QutipExperiment): QutipExperiment containing the compiled experiment for Qutip
-            converted_args (TaskArgsQutip): args of analog layer are converted to args for QuTip.
-
-        """
-        # pass to canonicaliza the operators in the AnalogCircuit
-        canonicalized_circuit = analog_operator_canonicalization(task.program)
-
-        # This just canonicalizes the operators inside the TaskArgsAnalog
-        # i.e. operators for Expectation
-        canonicalized_args = analog_operator_canonicalization(task.args)
-
-        # another pass which assigns the n_qreg and n_qmode of the
-        # AnalogCircuit IR
-        assigned_circuit = assign_analog_circuit_dim(canonicalized_circuit)
-
-        # This just verifies that the operators in the args have the same
-        # dimension as the operators in the AnalogCircuit
-        verify_analog_args_dim(
-            canonicalized_args,
-            n_qreg=assigned_circuit.n_qreg,
-            n_qmode=assigned_circuit.n_qmode,
-        )
-
-        # another pass which compiles AnalogCircuit to a QutipExperiment
-        converted_circuit = compiler_analog_circuit_to_qutipIR(
-            assigned_circuit, fock_cutoff=task.args.fock_cutoff
-        )
-
-        # This just converts the args so that the operators of the args are
-        # converted to qutip objects
-        converted_args = compiler_analog_args_to_qutipIR(canonicalized_args)
-
-        return (
-            converted_circuit,
-            converted_args,
-        )
+    """ 
 
     def run(
         self,
@@ -105,17 +53,13 @@ class QutipBackend(BackendBase):
         Note:
             only one of task or experiment must be provided.
         """
+        
+        circuit = task.program.circuit
+        cfg = task.program.cfg
+        symbol_table = task.program.symbol_table
+        
+        circuit, cfg, n_qreg, n_qmode = compile_analog_circuit(circuit=circuit, cfg=cfg, symbol_table=symbol_table)
 
-        # if experiment is None and args is not None:
-        #     raise TypeError("args provided without QuTip experiment")
-        # if experiment is not None and args is None:
-        #     raise TypeError("QuTip experiment provided without args")
-        #
-        # if task is not None and experiment is not None:
-        #     raise TypeError("Both task and experiment are given as inputs to run")
-        # if experiment is None:
-        #     experiment, args = self.compile(task=task)
+        # another pass which compiles AnalogCircuit to a QutipExperiment
+        return compiler_analog_circuit_to_qutip_backend(model=circuit, args=task.args, n_qreg=n_qreg, n_qmode=n_qmode)
 
-        experiment, args = self.compile(task=task)
-
-        return run_qutip_experiment(model=experiment, args=args)
---
+++ tests/test_qutip_simple.py
@@ -0,0 +1,106 @@
+# Copyright 2024-2025 Open Quantum Design
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+
+from oqd_core.interface.analog.expr import (
+    Annihilation,
+    Creation,
+    Identity,
+    PauliI,
+    PauliX,
+    PauliY,
+    PauliZ,
+)
+from oqd_core.backend.metric import Expectation
+from oqd_core.backend.task import Task, TaskArgsAnalog
+from oqd_core.backend.program import AnalogProgram
+from oqd_analog_emulator.qutip_backend import QutipBackend
+from oqd_core.frontend.analog import parse_analog
+
+
+
+from oqd_core.analysis.analog.cfg import AnalogCFGBuilder
+from oqd_core.analysis.analog.type_checker import AnalogTypeChecker
+from oqd_core.analysis.analog.symbol_table import AnalogSymbolTableBuilder
+
+
+@pytest.fixture
+def get_program():
+    # printer = Post(PrettyPrint())
+    
+    source = "r = qreg(1) \n H = -(3.14 / 4) %* %X \n  evolve(H, 1, r)"
+
+    circuit = parse_analog(source)
+    cfg = AnalogCFGBuilder().run(circuit)
+    checker = AnalogTypeChecker(cfg)
+    symbol_analysis = AnalogSymbolTableBuilder(cfg, checker.dataflow_result)
+    symbol_table = symbol_analysis.symbol_table
+
+    program = AnalogProgram(circuit=circuit, cfg=cfg, symbol_table=symbol_table)
+    return program
+
+X, Y, Z, PI, A, C, LI = (
+    PauliX(),
+    PauliY(),
+    PauliZ(),
+    PauliI(),
+    Annihilation(),
+    Creation(),
+    Identity(),
+)
+
+
+def get_amplitude_arrays(state: list):
+    real_amplitudes, imag_amplitudes = [], []
+    for x in state:
+        real_amplitudes.append(x.real)
+        imag_amplitudes.append(x.imag)
+    return real_amplitudes, imag_amplitudes
+
+
+def assert_lists_close(list1, list2, tolerance=0.001):
+    assert len(list1) == len(list2), "The input lists have different length"
+    for i, (elem1, elem2) in enumerate(zip(list1, list2)):
+        assert (
+            abs(elem1 - elem2) <= tolerance
+        ), "List elements {i}, {elem1} and {elem2}, are out of tolerance"
+
+
+def test_one_qubit_rabi_flopping(get_program):
+    """One qubit rabi flopping"""
+    args = TaskArgsAnalog(
+        n_shots=100,
+        fock_cutoff=4,
+        metrics={
+            "Z": Expectation(operator=Z),
+        },
+        dt=1e-3,
+    )
+
+    task = Task(program=get_program, args=args)
+
+    backend = QutipBackend()
+
+    results = backend.run(task=task)
+    print(results)
+
+    # real_amplitudes, imag_amplitudes = get_amplitude_arrays(results.state)
+
+    # assert_lists_close(real_amplitudes, [-0.707, 0])
+    # assert_lists_close(imag_amplitudes, [0, 0.707])
+    # assert abs(results.metrics["Z"][-1] - 0) <= 0.001
+    
+
